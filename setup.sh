39637ec342d868423cf494e9c71967c356bebbd2
Quinn Thibeault <quinn.thibeault96@gmail.com>
2021-03-23 10:47:01 -0700

    Fix variable declaration for BASEDIR

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-BASEDIR = $(dirname "$0")
+BASEDIR=$(dirname "$0")
 
 command_exists () {
-    if command -v $1 &>; then
+    if command -v $1 &>1
+    then
         return 0
     else
         return 1
@@ -11,7 +12,8 @@ command_exists () {
 }
 
 link_file () {
-    if [ -z $2 ]; then
+    if [ -z $2 ]
+    then
         ln -s $BASEDIR/$1 $HOME/$1
     else
         ln -s $BASEDIR/$1 $HOME/$2
@@ -19,7 +21,8 @@ link_file () {
 }
 
 setup_vim () {
-    if [ ! "$(ls -A vim/plugins)" ]; then
+    if [ ! "$(ls -A vim/plugins)" ]
+    then
         echo "Please checkout git submodules with command [git submodule --init --recursive]"
         exit 1
     fi
@@ -30,10 +33,12 @@ setup_vim () {
 }
 
 setup_ohmyzsh () {
-    if ! command_exists curl; then
+    if ! command_exists curl
+    then
         echo "Curl required for ZSH setup"
         exit 1
-    elif ! command_exists git; then
+    elif ! command_exists git
+    then
         echo "Git required for ZSH setup"
         exit 1
     fi
@@ -57,7 +62,8 @@ setup_git () {
 link_file .profile
 
 # Vim configuration
-if ! command_exists vim; then
+if ! command_exists vim
+then
     echo "Vim not found on system, skipping setup"
 else
     setup_vim
@@ -65,14 +71,16 @@ fi
 
 
 # Zsh configuration
-if ! command_exists zsh; then
+if ! command_exists zsh
+then
     echo "Zsh not found on system, skipping setup"
 else
     setup_zsh
 fi
 
 # Git configuration
-if ! command_exists git; then
+if ! command_exists git
+then
     echo "Git not found on system, skipping setup"
 else
     setup_git
