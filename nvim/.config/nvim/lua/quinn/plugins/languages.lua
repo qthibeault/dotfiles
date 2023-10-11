94a774bdfdf871d1b92bbaadb69f5851b7aab72e
Quinn Thibeault <quinn.thibeault96@gmail.com>
2023-10-11 08:46:00 -0700

    Move from vsnip to luasnip

--- nvim/.config/nvim/lua/quinn/plugins/languages.lua
+++ nvim/.config/nvim/lua/quinn/plugins/languages.lua
@@ -13,19 +13,22 @@ return {
         "hrsh7th/nvim-cmp",
         event = { "InsertEnter" },
         dependencies = {
+            {
+                "L3MON4D3/LuaSnip",
+                version = "2.*",
+            },
+            { "saadparwaiz1/cmp_luasnip" },
             { "hrsh7th/cmp-buffer" },
             { "hrsh7th/cmp-path" },
             { "hrsh7th/cmp-cmdline" },
             { "hrsh7th/cmp-nvim-lsp" },
-            { "hrsh7th/cmp-vsnip" },
-            { "hrsh7th/vim-vsnip" },
         },
         config = function()
             local cmp = require("cmp")
             local cmp_opts = {
                 snippet = {
                     expand = function(args)
-                        vim.fn["vsnip#anonymous"](args.body)
+                        require("luasnip").lsp_expand(args.body)
                     end,
                 },
                 window = {
@@ -38,28 +41,28 @@ return {
                     ["<C-Space>"] = cmp.mapping.complete(),
                     ["<C-e>"] = cmp.mapping.abort(),
                     ["<CR>"] = cmp.mapping.confirm({ select = true }),
-                    ["<Tab>"] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_next_item()
-                        elseif vim.fn["vsnip#available"](1) == 1 then
-                            feedkey("<Plug>(vsnip-expand-or-jump)", "")
-                        elseif has_words_before() then
-                            cmp.complete()
-                        else
-                            fallback()
-                        end
-                    end, { "i", "s" }),
-                    ["<S-Tab>"] = cmp.mapping(function()
-                        if cmp.visible() then
-                            cmp.select_prev_item()
-                        elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-                            feedkey("<Plug>(vsnip-jump-prev)", "")
-                        end
-                    end, { "i", "s" }),
+                    -- ["<Tab>"] = cmp.mapping(function(fallback)
+                    --     if cmp.visible() then
+                    --         cmp.select_next_item()
+                    --     elseif vim.fn["vsnip#available"](1) == 1 then
+                    --         feedkey("<Plug>(vsnip-expand-or-jump)", "")
+                    --     elseif has_words_before() then
+                    --         cmp.complete()
+                    --     else
+                    --         fallback()
+                    --     end
+                    -- end, { "i", "s" }),
+                    -- ["<S-Tab>"] = cmp.mapping(function()
+                    --     if cmp.visible() then
+                    --         cmp.select_prev_item()
+                    --     elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+                    --         feedkey("<Plug>(vsnip-jump-prev)", "")
+                    --     end
+                    -- end, { "i", "s" }),
                 }),
                 sources = cmp.config.sources({
                     { name = "nvim_lsp" },
-                    { name = "vsnip" },
+                    { name = "luasnip" },
                 }, {
                     { name = "buffer" },
                 }),
