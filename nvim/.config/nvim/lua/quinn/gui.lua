54009375268f8dee2aa432e3d8d3c7ddae78c744
Quinn Thibeault <quinn.thibeault96@gmail.com>
2024-07-30 12:11:04 -0700

    Create module for GUI configuration options

---
+++ nvim/.config/nvim/lua/quinn/gui.lua
@@ -0,0 +1,6 @@
+if vim.g.neovide then
+    vim.o.guifont = "JetBrainsMono Nerd Font Mono:h14"
+    vim.g.neovide_theme = "auto"
+    vim.g.neovide_cursor_animation_length = 0
+    vim.g.neovide_cursor_vfx_mode = ""
+end
